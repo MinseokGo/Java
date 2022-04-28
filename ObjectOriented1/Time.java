37eb5280cd735a30161b1ddb1d423335f86ef337
MinseokGO <96585636+MinseokGo@users.noreply.github.com>
2022-04-28 20:00:06 +0900

    Update Time.java

--- ObjectOriented1/Time.java
+++ ObjectOriented1/Time.java
@@ -9,17 +9,17 @@ public class Time {
 	
 	public void setHour(int h) {
 		if(h < 0 || h > 23) return;
-		hour = h;
+			hour = h;
 	}
 	
 	public void setMinute(int m) {
 		if(m < 0 || m > 59) return;
-		minute = m;
+			minute = m;
 	}
 	
 	public void setSecond(float s) {
 		if(s < 0.0f || s > 59.99f) return;
-		second = s;
+			second = s;
 	}
 }
 
