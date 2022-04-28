202d93e60f0af5ece71791c6ce34d63c42c30df5
MinseokGO <96585636+MinseokGo@users.noreply.github.com>
2022-04-28 21:14:29 +0900

    Update CallStackTest.java

--- ObjectOriented1/CallStackTest.java
+++ ObjectOriented1/CallStackTest.java
@@ -1,15 +1,18 @@
 class CallStackTest {
 	public static void main(String[] args) {
+		System.out.println("mainMethod start");
 		firstMethod();	// static 메서드는 객체 생성 없이 호출 가능
 		System.out.println("mainMethod exit");
 	}
 	
 	static void firstMethod() {
+		System.out.println("firstMethod start");
 		secondMethod();
 		System.out.println("firstMethod exit");
 	}
 	
 	static void secondMethod() {
+		System.out.println("secondMethod start");
 		System.out.println("secondMethod exit");
 	}
 }
