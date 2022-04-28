d6c709963f4acbb6976f3e0972b4ef23901c7527
MinseokGO <96585636+MinseokGo@users.noreply.github.com>
2022-04-28 22:40:14 +0900

    Update FactorialTest.java

--- ObjectOriented1/FactorialTest.java
+++ ObjectOriented1/FactorialTest.java
@@ -1,13 +1,21 @@
 class FactorialTest {
 	public static void main(String[] args) {
-		int result = factorial(4);
-		
-		System.out.printf("%d", result);
+		long result = 0;
+		int n = 21;
+		for(int i=1; i<=n; i++) {
+			result = factorial(i);
+			if(result == -1) {
+				System.out.println("유효하지 않은 값입니다.");
+				break;
+			}
+			System.out.printf("%2d!=%20d%n", i, result);
+		}
 	}
 	
 	static int factorial(int n) {
 		int result = 0;
 		
+		if(n <= 0 || n > 20) return -1;	// 매개변수 n의 유효성 검사
 		if(n == 1)
 			result = 1;
 		else 
